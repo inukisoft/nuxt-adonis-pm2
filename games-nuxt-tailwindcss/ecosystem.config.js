d37dc60baed3db9bdbcd7865045d4d28d461e0d8
Constantino Samuel Alarcon Mery <constantino.alarcon@sii.cl>
2021-04-26 02:56:25 -0400

    cambios

---
+++ games-nuxt-tailwindcss/ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = 
+{
+  apps: [
+    {
+      name: "NuxtGames",
+      exec_mode: "cluster",
+      instances: "2", 
+      script: "./node_modules/nuxt/bin/nuxt.js",
+      args: "start"
+    }
+  ],
+
+  deploy: {
+        "integracion" : {
+          "user" : "root",
+          "host" : ["felis.sii.cl"],
+          "ref"  : "origin/master",
+          "repo" : "http://cefio.sii.cl/arq/arsii/poc/nuxt-adonis-arsii.git",
+          "path" : "/APP01/pm2_deploy",
+          "ssh_options": "StrictHostKeyChecking=no",
+          "pre-setup" : "rm -rf /APP01/pm2_deploy/source ; ls -lart ",
+          "pre-deploy-local" : "echo 'This is a local executed command'",
+          "post-deploy" : "cd game-api-adonis ;  ls -lart ; npm i && npm run build && cp .env.integracion ./build &&  sudo pm2 startOrRestart ecosystem.config.js --env integracion"
+         }
+      
+  }  
+}
