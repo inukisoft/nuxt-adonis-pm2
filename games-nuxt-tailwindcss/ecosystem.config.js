e51010de849bf209afbe907c9aec4a2eed6754c8
Constantino Samuel Alarcon Mery <constantino.alarcon@sii.cl>
2021-04-25 20:19:53 -0400

    actualizando archivo ecosistema

--- games-nuxt-tailwindcss/ecosystem.config.js
+++ games-nuxt-tailwindcss/ecosystem.config.js
@@ -7,5 +7,18 @@ module.exports = {
         script: './node_modules/nuxt/bin/nuxt.js',
         args: 'start'
       }
-    ]
+    ],
+
+    deploy : {
+        // "production" is the environment name
+        "production" : {
+          "user" : "root",
+          "host" : ["felis.sii.cl"],
+          "ref"  : "origin/master",
+          "repo" : "http://cefio.sii.cl/arq/arsii/poc/nuxt-adonis-arsii",
+          "path" : "/APP01/pm2_deploy",
+          "post-deploy" : "cd games-nuxt-tailwindcss; npm i; npm run build ;"
+         },
+      }    
+
   }
