ca3dae1954cde8d971161fa8d5955571b128b042
Constantino Samuel Alarcon Mery <constantino.alarcon@sii.cl>
2021-04-25 22:10:09 -0400

    env integracion file

---
+++ game-api-adonis/ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    apps: [
+      {
+        name: 'ApiAdonisGames',
+        exec_mode: 'cluster',
+        instances: '2', // Or a number of instances
+        script: './node_modules/nuxt/bin/nuxt.js',
+        args: 'start'
+      }
+    ],
+
+    deploy : {
+        // "production" is the environment name
+        "integracion" : {
+          "user" : "root",
+          "host" : ["felis.sii.cl"],
+          "ref"  : "origin/master",
+          "repo" : "http://cefio.sii.cl/arq/arsii/poc/nuxt-adonis-arsii.git",
+          "path" : "/APP01/pm2_deploy",
+          "ssh_options": "StrictHostKeyChecking=no",
+          "pre-setup" : "ls -lart",
+          "pre-deploy-local" : "echo 'This is a local executed command'",
+          "post-deploy" : "cd game-api-adonis && npm i && npm run build && sudo pm2 startOrRestart ecosystem.config.js --name WEB -i 2"
+         },
+      }    
+
+  }
