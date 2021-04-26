7519a67d74cb8f008d3c669aaf1d1e8151c9b466
Constantino Samuel Alarcon Mery <constantino.alarcon@sii.cl>
2021-04-26 01:04:57 -0400

    cambio script

--- games-nuxt-tailwindcss/ecosystem.config.js
+++ games-nuxt-tailwindcss/ecosystem.config.js
@@ -20,7 +20,7 @@ module.exports = {
           "ssh_options": "StrictHostKeyChecking=no",
           "pre-setup" : "ls -lart",
           "pre-deploy-local" : "echo 'This is a local executed command'",
-          "post-deploy" : "cd games-nuxt-tailwindcss && npm i && npm run build && sudo pm2 startOrRestart ecosystem.config.js --name WEB -i 2 --env integracion"
+          "post-deploy" : "cd games-nuxt-tailwindcss && npm i && npm run build && sudo pm2 start ecosystem.config.js --name WEB -i 2 --env integracion"
          },
       }    
 
