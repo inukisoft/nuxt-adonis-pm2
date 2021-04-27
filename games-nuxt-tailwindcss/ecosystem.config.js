e3f0dc3ea84d506792317fdd0e271676f2bc7b75
Constantino Samuel Alarcon Mery <constantino.alarcon@sii.cl>
2021-04-27 19:52:33 -0400

    camvbios

--- games-nuxt-tailwindcss/ecosystem.config.js
+++ games-nuxt-tailwindcss/ecosystem.config.js
@@ -20,7 +20,7 @@ module.exports =
           "ssh_options": "StrictHostKeyChecking=no",
           "pre-setup" : "rm -rf /APP01/pm2_deploy/source ; ls -lart ",
           "pre-deploy-local" : "echo 'This is a local executed command'",
-          "post-deploy" : "cd games-nuxt-tailwindcss ;  ls -lart ; npm ci ; npm run build ; sudo pm2 startOrRestart ecosystem.config.js --env integracion"
+          "post-deploy" : "cd games-nuxt-tailwindcss ;  ls -lart ; npm install && npm run build && sudo pm2 startOrRestart ecosystem.config.js --env integracion"
          }
       
   }  
