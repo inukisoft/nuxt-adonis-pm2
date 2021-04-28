1e2d65aaa84c239c81616091ebb9170f2b09e58e
Ubuntu <ubuntu@ip-172-31-36-47.us-west-2.compute.internal>
2021-04-28 02:36:24 +0000

    configuraciones

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,13 +4,13 @@ module.exports = {
     {
       name: "API-AdonisGames",
       exec_mode: "cluster",
-      instances: "2",
+      instances: "1",
       script: "./game-api-adonis/build/server.js"
     },
     {
       name: "NuxtGames",
       exec_mode: "cluster",
-      instances: "2", 
+      instances: "1", 
       script: "./games-nuxt-tailwindcss/node_modules/nuxt/bin/nuxt.js",
       args: "start"
     }    
@@ -18,12 +18,12 @@ module.exports = {
   deploy: {
         "integracion" : {
           "user" : "root",
-          "host" : ["felis.sii.cl"],
-          "ref"  : "origin/master",
-          "repo" : "http://cefio.sii.cl/arq/arsii/poc/nuxt-adonis-arsii.git",
-          "path" : "/APP01/pm2_deploy",
+          "host" : ["localhost"],
+          "ref"  : "origin/main",
+          "repo" : "https://github.com/inukisoft/nuxt-adonis-pm2.git",
+          "path" : "/home/ubuntu/pm2_deploy",
           "ssh_options": "StrictHostKeyChecking=no",
-          "pre-setup" : "rm -rf /APP01/pm2_deploy/source",
+          "pre-setup" : "rm -rf /home/ubuntu/pm2_deploy/source",
           "pre-deploy-local" : "echo 'This is a local executed command'",
           "post-deploy" : "cd game-api-adonis && npm i && npm run build && cp .env.integracion build/.env && cd .. && cd games-nuxt-tailwindcss && npm install && npm run build && cd .. && sudo pm2 startOrRestart ecosystem.config.js --env integracion"
          }
